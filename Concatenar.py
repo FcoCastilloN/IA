f9c4317462e81facb3088420245debdd24806a24
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-16 17:05:54 -0600

    Practica3

---
+++ Concatenar.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_
+
+#Las 4 primeras lineas con para las palabras con acentos
+
+palabra_1 = "Hola"
+palabra_2 = "Mundo"
+nombre_completo = palabra_1 + " "+ palabra_2 + "." 
+print(nombre_completo)
