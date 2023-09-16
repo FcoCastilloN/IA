4fc6454cd41d1b1d9db8bc11bbd8b403fb2e6345
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-16 16:53:35 -0600

    Practica2

---
+++ Strings.py
@@ -0,0 +1,12 @@
+"Este es un string"
+'Este es un string'
+
+texto = "Este es un string"
+texto2 = 'Este es un string'
+texto3 = 'Este es un "string"'
+texto4 = "Este es un 'string'"
+
+print(texto)
+print(texto2)
+print(texto3)
+print(texto4)
