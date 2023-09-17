c2f9ec952b442a1ad2d9019ae7640b15ccd1c7b4
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-16 22:19:05 -0600

    Practica18

---
+++ Cómo_funcionan_las_tuplas.py
@@ -0,0 +1,8 @@
+lista = ['rojo', 'azul', 'verde', 'amarillo']
+tupla = ('rojo', 'azul', 'verde', 'amarillo')
+
+tupla2= (20,15,10, "El resultado es: ")
+
+print(lista) #se imprime la lista
+print(tupla)  #se imprime la tupla
+print(tupla2[3], tupla2[0] + tupla2[1]) # se realiza las imdicaciones a una tupla
