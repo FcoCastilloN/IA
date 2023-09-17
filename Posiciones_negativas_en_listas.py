de3473a6285ccf1df726691bca8b00a9df4d0d7f
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-16 20:59:49 -0600

    Practica10

---
+++ Posiciones_negativas_en_listas.py
@@ -0,0 +1,6 @@
+colores = ['rojo','azul','verde','amarillo','marron',
+           'lila','negro','rosa','blanco','naranja']
+
+print("El ultimo color de la lista es: " + colores[-1])  #con los numeros negativos nos 
+                                                       # dirigimos del ultimo hacia el primero
+
