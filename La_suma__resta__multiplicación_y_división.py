09ba477db107f999a87a7dc538a4c78a0ade3460
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-16 19:39:48 -0600

    Practica6

---
+++ La_suma__resta__multiplicación_y_división.py
@@ -0,0 +1,10 @@
+suma = 4 + 16
+resta = 9 - 6
+multiplicacion = 4 * 3
+division = 12/2
+
+print(suma + resta + multiplicacion + division)  #aqui no mostrara el resultado de cada operacion, sino 
+                                                 #que la suma de cada operacion
+
+print(suma, resta, multiplicacion, division)     #aqui muestra el resultado de cada operacion
+                                                 #despues de cada ,
