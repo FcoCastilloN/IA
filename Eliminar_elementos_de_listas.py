45e16cac866735fa972b39160633175a690bff0c
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-16 21:10:30 -0600

    Practica11

---
+++ Eliminar_elementos_de_listas.py
@@ -0,0 +1,7 @@
+colores = ['rojo','azul','verde','amarillo','marron',
+           'lila','negro','rosa','blanco','naranja']
+
+del colores[0]   # puedes eliminar el color que tu elijas. ya sea el ultimo o el primero
+del colores[-1]  #o cualquier otro
+
+print(colores) 
