c68f2db1b72b80a9395d3211af740c87ef56df52
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-17 17:22:21 -0600

    Practica23

---
+++ Buscar_datos_en_listas_y_tuplas.py
@@ -0,0 +1,5 @@
+entrada = input("tu navegador favorito:\n")
+
+navegadores = ['chrome', 'firefox', 'opera', 'safari']   # se genera la lista
+
+print('chrome' in navegadores)  #busca en la lista si se encuentra lo que solicitamos
