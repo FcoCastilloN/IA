34fd4133f87b3f942b4d0bcaa3f9627bb8acb8ad
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-16 21:28:37 -0600

    Practica13

---
+++ Eliminar_elementos_de_listas_Python_con_el_método_pop__.py
@@ -0,0 +1,10 @@
+
+colores   = [ 'rojo' , 'azul' , 'verde' , 'amarillo' , 'marron' ,
+           'lila' , 'negro' , 'rosa' , 'blanco' , 'naranja' ]
+
+guardaLista = colores.pop(2)  #borra el elemento seleccionado de la lista y lo guarda
+                              #en una variable
+
+print ( colores )             #imprime la lista sin el el que se le quito
+
+print(guardaLista)            #imprime el elemento que se quito y que se guardo en una variable 
