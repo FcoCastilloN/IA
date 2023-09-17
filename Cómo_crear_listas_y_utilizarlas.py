ced1dd852c8394959cda82b877025a1dbcd82096
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-16 20:38:57 -0600

    Practica9

---
+++ Cómo_crear_listas_y_utilizarlas.py
@@ -0,0 +1,5 @@
+tenis = ("NIKE", "ADIDAS", "PUMA", "CHARLIE")
+
+print(tenis)  #nos da los datos de toda la lista
+
+print(tenis[2])  #nos da como resultado cierto numero que mandemos a llamar de la lista
