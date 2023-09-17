ccb6bf5f39c92474922a92699f4a89a1186df21b
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-16 21:33:56 -0600

    Practica14

---
+++ Insertar_elementos_en_listas.py
@@ -0,0 +1,7 @@
+
+colores    = [ 'rojo' , 'azul' , 'verde' , 'amarillo' , 'marron']
+
+colores.append("naranja") #se agrega a la lista el elemento deseado en la ultima posicion,
+                          #esto funciona si tiene o no tiene elementos nuestra lista
+
+print(colores)
