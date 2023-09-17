5eeafa2ee66ffe5b0c0507c7835732f5940d887f
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-16 21:46:47 -0600

    Practica16

---
+++ Ordenamiento_de_elementos_en_listas.py
@@ -0,0 +1,8 @@
+colores  = [ 'rojo' , 'azul' , 'verde' , 'amarillo' ]
+colores2  = [ 'rojo' , 'azul' , 'verde' , 'amarillo' ]
+
+colores.sort()  #ordena los elementos de A-Z
+colores2.sort(reverse=True)  #ordena los elementos de Z-A
+
+print(colores)
+print(colores2)
