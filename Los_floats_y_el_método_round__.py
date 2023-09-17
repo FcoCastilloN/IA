87cdc521c679139e15ab402cccbc8730176b5882
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-16 20:27:11 -0600

    Practica8

---
+++ Los_floats_y_el_método_round__.py
@@ -0,0 +1,9 @@
+num1 = 40.23
+
+num2 = 23.4132
+
+resultado = num1 + num2
+print(num1+num2)   #realiza la operacion sin tener un limite de decimales
+
+print(round(resultado,2))   # la opcion round limita el numero de decimales, en este caso
+                            # lo limitamos a 2 decimales
