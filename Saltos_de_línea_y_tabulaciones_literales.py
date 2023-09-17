0c545a735e35507150b4a4835d7f54313f2b2fbc
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-16 19:27:21 -0600

    Practica5

---
+++ Saltos_de_línea_y_tabulaciones_literales.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_
+
+print("Practicas\nde programacion\nen Python")  #realiza un salto de linea despues de cada \n
+
+print("\tPracticas\n\tde programacion\n\ten Python")  #realiza una tabulacion despues de cada \t
