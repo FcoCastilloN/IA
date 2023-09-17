3add8a9bad280a9d29b9cad1be5d7b3fcafd780e
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-16 21:18:54 -0600

    Practica12

---
+++ Eliminar_elementos_en_listas_Python_con_remove__.py
@@ -0,0 +1,7 @@
+colores  = [ 'rojo' , 'azul' , 'verde' , 'amarillo' , 'marron' ,
+           'lila' , 'negro' , 'rosa' , 'blanco' , 'naranja' ]
+
+colores.remove("azul")  #se elimina lo que se le asigne
+colores.remove("verde")  #se pueden eliminar varios
+
+print ( colores )
