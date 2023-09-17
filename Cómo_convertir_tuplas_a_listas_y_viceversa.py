4428d7bffd09d13c5c87e02a7790729cfb620513
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-16 22:25:00 -0600

    Practica19

---
+++ Cómo_convertir_tuplas_a_listas_y_viceversa.py
@@ -0,0 +1,8 @@
+lista = ['rojo', 'azul', 'verde', 'amarillo']
+tupla = ('rojo', 'azul', 'verde', 'amarillo')
+
+tupla2 = tuple(lista) #convierte la lista a una tupla
+lista = list(tupla)   #convierte la tupla a una lista
+
+print(tupla2)
+print(lista)
