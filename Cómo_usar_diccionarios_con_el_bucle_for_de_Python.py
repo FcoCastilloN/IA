d37161b6cf00bfeba6439ed5792ed16aa44b7ecb
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-17 20:52:05 -0600

    Practica31

---
+++ Cómo_usar_diccionarios_con_el_bucle_for_de_Python.py
@@ -0,0 +1,14 @@
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+teclado1['Precio'] = '85'
+print(teclado1['Precio'])
