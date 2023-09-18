6b68189f79244cc9b1feb240503e6ad86271ff5d
FcoCastilloN <145232024+FcoCastilloN@users.noreply.github.com>
2023-09-17 20:28:33 -0600

    Practica28

---
+++ El_bucle_for_de_Python.py
@@ -0,0 +1,7 @@
+for x in 'Python':
+    print(x)     #se realiza un salto de linea en cada letra
+
+cursos = ['uno','dos','tres','cuatro']
+
+for x in cursos:  #se relaiza un salto de linea en cada palabra
+	print(x)
